63f8ae176a9c38d6711ed22b0e8e6bd0aa76993a
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-11 12:45:56 +0100

    Add toggle functionality to answerButton 2 / Change Text

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ const bookmarkButton = document.querySelector('[data-js="button-bookmark"]');
 // add eventListener for toggle
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active"); // toggle classes bookmark & bookmark--active
-  console.log(bookmarkButton);
 });
+
+console.log("BOOKMARK BUTTON_", bookmarkButton);
 
 // TOGGLE ANSWER BUTTON
 
-// select answerButton
+// select answerButton and cardAnswer
 const answerButton = document.querySelector('[data-js="button-answer"]');
 const cardAnswer = document.querySelector('[data-js="card-answer"]');
 
 // add eventListener for toggle
 answerButton.addEventListener("click", () => {
   cardAnswer.classList.toggle("card__answer--active");
-  console.log(cardAnswer);
+  // toggle textContent
+  if (cardAnswer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
 });
+
+console.log("ANSWER BUTTON_", answerButton);
+console.log("CARD ANSWER_", cardAnswer);
