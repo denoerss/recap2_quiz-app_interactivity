3e36346b90b077e08cac5a1c0cc9b9c7400e6d1b
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-11 19:36:29 +0100

    Read all entered data from the input fields

--- components/form/form.js
+++ components/form/form.js
@@ -7,4 +7,18 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   // Prevent default submit behavior
   event.preventDefault();
+
+  // Read all entered data from the input fields (question, answer, tags)
+  const formData = new FormData(form);
+  console.log("FORM DATA_", formData);
+
+  // Save all entered data from the input fields in variables
+  const question = formData.get("question");
+  const answer = formData.get("answer");
+  const tag = formData.get("tag");
+
+  // Log all collected data
+  console.log("QUESTION_", question);
+  console.log("ANSWER_", answer);
+  console.log("TAG_", tag);
 });
